ce83a9d60dcd6c6b4afde15ad22c12f089f6075f
seethalakshmipl <50530488+seethalakshmipl@users.noreply.github.com>
2019-05-19 19:56:15 +0200

    Bug fixes and enhancements

--- my-vueapp/src/components/ViewPage.vue
+++ my-vueapp/src/components/ViewPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main_container" id="container">
+  <div class="main_container">
     <!-- Header Image Container -->
     <div class="jumbotron">
       <div class="container header_container">
@@ -17,37 +17,41 @@
     <!-- Test Package Container -->
     <div class="container-fluid text-center test_package_container">
       <div class="container-fluid">
-        <!-- Left-aligned media object -->
         <div class="row test_package_row">
           <div class="col-lg-7 col-md-7 mb-20">
             <div class="test_package_img_div">
-              <img src="../assets/images/lillydoo-testpaket-10.jpg" class="testpaket_img"  id="example">
+              <img :src="imgurl" class="testpaket_img" id="img_id">
               <img src="../assets/images/oekotex_de.png" class="oekotex_img">
             </div>
           </div>
           <div class="col-lg-5 col-md-5 mb-20 text-left">
             <h3>Unser gratis Testpaket</h3>
             <p>WÄHLE DEINE GRÖSSE</p>
-            <div class="row">
-              <button class="selective_btn" @click="changeImage(1)">
-                1
-                <br>(2-3 KG)
+            <div class="row" id="selective_btn_id">
+              <button class="selective_btn active" @click="changeImage(10)">
+                <span class="selective_btn_span">1</span>
+                <br>
+                <span class="selective_btn_kg_span">(2-3 KG)</span>
               </button>
-              <button class="selective_btn" @click="changeImage(2)">
-                2
-                <br>(3-4 KG)
+              <button class="selective_btn" @click="changeImage(20)">
+                <span class="selective_btn_span">2</span>
+                <br>
+                <span class="selective_btn_kg_span">(3-4 KG)</span>
               </button>
-              <button class="selective_btn" @click="changeImage(3)">
-                3
-                <br>(4-7 KG)
+              <button class="selective_btn" @click="changeImage(30)">
+                <span class="selective_btn_span">3</span>
+                <br>
+                <span class="selective_btn_kg_span">(4-7 KG)</span>
               </button>
-              <button class="selective_btn" @click="changeImage(4)">
-                4
-                <br>(7-10 KG)
+              <button class="selective_btn" @click="changeImage(40)">
+                <span class="selective_btn_span">4</span>
+                <br>
+                <span class="selective_btn_kg_span">(7-10 KG)</span>
               </button>
-              <button class="selective_btn" @click="changeImage(5)">
-                5
-                <br>(10-12 KG)
+              <button class="selective_btn" @click="changeImage(50)">
+                <span class="selective_btn_span">5</span>
+                <br>
+                <span class="selective_btn_kg_span">(10-12 KG)</span>
               </button>
             </div>
             <p>Teste jetzt unsere Windeln und Feuchttücher - In Größe 1 - 3 enthält unser Testpaket unsere Feuchttücher mit 99 % Wasser, ab Gr. 4 erhältst Du unsere Sensitiven Feuchttücher. Wir zahlen die Produkte, Du nur den Versand.</p>
@@ -124,7 +128,6 @@
         <div class="row diaper_row">
           <h1>Dein Testpaket enthält</h1>
         </div>
-        <!-- Left-aligned media object -->
         <div class="row diaper_row">
           <div class="col-lg-6 col-md-6 mb-20">
             <div class="media">
@@ -146,13 +149,15 @@
           <div class="col-lg-6 col-md-6 mb-20">
             <div class="media">
               <div class="media-left">
-                <img src="../assets/images/water-wipes-15-small (1).jpg" class="media-object">
+                <img :src="waterWipesImgurl" class="media-object" id="waterWipesImg_id">
               </div>
               <div class="media-body text-left">
-                <h4 class="media-heading">15 FEUCHTTÜCHER MIT 99 % WASSER</h4>
+                <h4 class="media-heading" id="waterWipesText_id">15 FEUCHTTÜCHER MIT 99 % WASSER</h4>
                 <ul class="diaper_ul">
                   <li>0 % Parfüme & PEGs, 100 % biologisch abbaubar</li>
-                  <li>Natürlich rein, extra mild, Alternative zu "Wasser & Watte"</li>
+                  <li
+                    id="waterWipesNaturalText_id"
+                  >Natürlich rein, extra mild, Alternative zu "Wasser & Watte"</li>
                 </ul>
               </div>
             </div>
@@ -160,17 +165,67 @@
         </div>
       </div>
     </div>
-    <!-- Diaper Container -->
   </div>
 </template>
 <script>
-export default{
-  methods: {
-    changeImage(event) {
-      console.log(event);
-        document.getElementById("example").className = "MyClass";
+export default {
+  data: function() {
+    return {
+      imgurl: require('../assets/images/lillydoo-testpaket-10.jpg'),
+      waterWipesImgurl: require('../assets/images/water-wipes-15-small (1).jpg')
     }
   },
+
+  methods: {
+    // Change Image and text Function
+    changeImage(event) {
+      // TestPacket Image Change Variables
+
+      var path = require('../assets/images/lillydoo-testpaket-' +
+        event +
+        '.jpg')
+      document.getElementById('img_id').src = path
+
+      // Diaper Image and Text Change variables
+
+      var waterWipesPath = require('../assets/images/sensitive-wipes-15-small.jpg')
+      var waterWipes_text = '15 FEUCHTTÜCHER MIT 99 % WASSER'
+      var sensitiveWipes_text = '15 SENSITIVE FEUCHTTÜCHER'
+      var waterWipesNatural_text =
+        'Natürlich rein, extra mild, Alternative zu "Wasser & Watte"'
+      var waterWipesThick_text = 'Extra dickes und kompostierbares Tuch'
+
+      // Diaper Image and Text Change Functions
+      if (event === 40 || event === 50) {
+        document.getElementById('waterWipesImg_id').src = waterWipesPath
+        document.getElementById(
+          'waterWipesText_id'
+        ).innerHTML = sensitiveWipes_text
+        document.getElementById(
+          'waterWipesNaturalText_id'
+        ).innerHTML = waterWipesThick_text
+      } else {
+        document.getElementById(
+          'waterWipesImg_id'
+        ).src = require('../assets/images/water-wipes-15-small (1).jpg')
+        document.getElementById('waterWipesText_id').innerHTML = waterWipes_text
+        document.getElementById(
+          'waterWipesNaturalText_id'
+        ).innerHTML = waterWipesNatural_text
+      }
+
+      // Active Class for highlighting button color
+      var selectiveButton = document.getElementById('selective_btn_id')
+      var buttons = selectiveButton.getElementsByClassName('selective_btn')
+      for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+          var current = document.getElementsByClassName('active')
+          current[0].className = current[0].className.replace(' active', '')
+          this.className += ' active'
+        })
+      }
+    }
+  }
 }
 </script>
 <style>
